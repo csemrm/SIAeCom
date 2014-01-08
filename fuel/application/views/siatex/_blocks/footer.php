690cfddda2bed939cbbf4f222d324965f22e3166
Md. Mostafizur Rahman <csemrm@gmail.com>
2014-01-08 20:14:36 +0600

    layout added

---
+++ fuel/application/views/siatex/_blocks/footer.php
@@ -0,0 +1,93 @@
+</div>
+ 
+<!--Body-->
+
+<!--Footer-->
+<div id="footer_contener"> 
+
+    <div class="fotter">
+        <div class="fotter_conten_here">
+            <div class="fotter_box">
+                <ul>
+                    <li><a href="#">Shipping <br> On all orders</a></li>
+                </ul>
+            </div>
+            <div class="fotter_box fotter_box2">
+                <ul>
+                    <li><a href="#">Shipping <br> On all orders</a></li>
+                </ul>
+            </div>
+            <div class="fotter_box box">
+                <ul>
+                    <li><a href="#"> Guaranteed <br> delivery date</a></li>
+                </ul>
+            </div>
+            <div class="fotter_box list">
+                <ul>
+                    <li><a href="#">Quality printing & <br> embrodery </a></li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+
+
+</div>
+<div class="footer_bg">
+    <div class="footer_tex">
+        <div class="fore_box">
+            <h5>Our menu</h5>
+            <ol class="footnotes">
+                <li>&raquo;  <span><a href="index.html">Home</a></span></li>
+                <li>&raquo;  <span><a href="products.html">Products</a></span></li>
+                <li>&raquo; <span><a href="services.html">Services</a></span></li>
+                <li>&raquo; <span><a href="contact.html">Contact us</a></span></li>
+            </ol>                                       
+        </div>
+        <div class="fore_box">
+            <h5>Head Office</h5>
+            <ol class="footnotes">
+                <li><span>Siatex Bangladesh Limited</span></li>
+                <li> <span>House # 8, Road # 6, BLK "E"</span></li>
+                <li><span>Niketon,Gulshan -1,Dhaka</span></li>
+                <li> <span>Phone: (+880-2) 985-9720</span></li>
+            </ol>     
+        </div>
+        <div class="fore_box">
+            <h5>Sales Office </h5>
+            <ol class="footnotes">
+                <li><span>Siatex Bangladesh Limited</span></li>
+                <li> <span>House # 8, Road # 6, BLK "E"</span></li>
+                <li><span>Niketon,Gulshan -1,Dhaka</span></li>
+                <li> <span>Phone: (+880-2) 985-9720</span></li>
+            </ol>     
+        </div>
+        <div class="fore_box">
+            <h5>Canada Sales Office </h5>
+            <ol class="footnotes">
+                <li> <span>APTEX Canada Limited</span></li>
+                <li> <span>22 Hagley Road</span></li>
+                <li> <span>Toronto, M1M 1S9</span></li>
+                <li> <span>ON Canada</span></li>
+            </ol>     
+        </div>
+
+
+
+    </div>
+    <div class="copy_right">Copyright © 2013  Dutta Fashion. All rights reserved. 
+
+        <span class="bottom_menu"><a href="#">Contact Us </a> |     <a href="#">About Us</a></span>
+
+    </div>
+
+</div>
+<!--Footer-->
+
+</div>
+<?php echo js('jquery,main') . js($js); ?>
+</body>
+</html>
+
+
+
---
+++ fuel/application/views/siatex/_blocks/header.php
@@ -0,0 +1,82 @@
+<?php if (empty($assets_path)) $assets_path = 'assets/'; ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta charset="utf-8">
+            <title>
+                <?php
+                if (!empty($page_title)) {
+                    echo $page_title;
+                }
+                ?>
+            </title>
+
+            <meta name="keywords" content="<?php
+            if (!empty($page_title)) {
+                echo $page_title;
+            }
+            ?>">
+                <meta name="description" content="<?php
+                if (!empty($page_title)) {
+                    echo $page_title;
+                }
+                ?>">
+                          <?php
+                          echo css('siatex/style,siatex/screen') . css($css);
+                          ?>
+
+
+                    <link href='http://fonts.googleapis.com/css?family=News+Cycle|Anaheim|Open+Sans+Condensed:300' rel='stylesheet' type='text/css'/>
+                    <script type="text/javascript" src="fuel/modules/fuel/assets/js/jquery/jquery.js"></script>	
+
+                    </head>
+
+                    <body>
+
+                        <div id="conten">
+
+                            <!--Top Bar-->
+
+                            <div id="top_two_pix"></div>
+                            <div id="top_menu">
+                                <div class="top_menu_tex">
+                                    <div class="left_contact_icon">
+                                        <ul>
+                                            <li class="email_icon email_icon2"><a href="<?php echo site_url(); ?>">Home</a></li>
+                                            <li class="phone_icon email_icon1">+ 123 456 1789</li>
+                                              <!--<li class="phone_icon2">Language <select>
+                                              <option>English </option> 
+                                              <option>Italian</option>
+                                               </select></li>-->
+                                        </ul>
+                                    </div>
+                                    <div class="left_social_icon">
+                                        <ul>
+                                            <li><a title="facebook" href="#"><img src="<?php echo $assets_path; ?>images/facebook.png" alt="facebook" width="7" height="14" border="0" /></a></li>
+                                            <li><a title="twitter" href="#"><img src="<?php echo $assets_path; ?>images/twitter.png" alt="twitter" width="19" height="13" border="0" /></a></li>
+                                            <li><a title="linkedin" href="#"><img src="<?php echo $assets_path; ?>images/linkedin.png" alt="linkedin" width="13" height="13" border="0" /></a></li>
+                                            <li><a title="rss" href="#"><img src="<?php echo $assets_path; ?>images/rss.png" alt="rss" width="13" height="12" border="0" /></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <!--Top Bar-->
+
+
+                            <!--Hader-->
+
+                            <div id="hader">
+                                <div id="logo"><a title="Dutta Fashion" href="#"><img src="<?php echo $assets_path; ?>images/logo.png" alt="Dutta Fashion" width="192" height="126" border="0" /></a></div>
+                                <div id="right_menu">
+                                    <ul>
+                                        <li class="home_icon" ><a href="<?php echo site_url(); ?>"><span>Home</span></a></li>
+                                        <li class="products"><a href="products.html">Products</a></li>
+                                        <li class="services"><a href="services.html">services</a></li>
+                                        <li class="contact"><a href="contact.html">contact</a></li>
+                                    </ul>
+
+                                </div>
+                            </div>
+                            <div id="body">
